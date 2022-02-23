bb555254d0f92354a0d0937e0a8ea31162a9ed36
Hgaddy <holdengaddy@gmail.com>
2022-02-23 01:20:29 -0600

    Added sprite and random movement to ScreensaverHeros.

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2,24 +2,38 @@ package;
 
 import flixel.FlxState;
 import Hero;
+import ScreensaverHero;
 
 class PlayState extends FlxState
 {
 	var hero:Hero;
+	var screensaver:ScreensaverHero;
+	var x:Int = 0;
 
 	override public function create()
 	{
 		super.create();
 
+		//make Hero
 		hero = new Hero(300,300);
 		add(hero);
 
+		//make Text
 		var text = new flixel.text.FlxText(0, 0, 0, "Hello World", 64);
 		text.screenCenter();
 		text.borderColor = 0xFFFFC0CB;
 		text.borderSize = 200;
 		text.borderStyle = SHADOW;
 		add(text);
+
+		//make ScreensaverHero
+		while (x < 10)
+		{
+			screensaver = new ScreensaverHero(300,300);
+			add(screensaver);
+			x++;
+		}
+		
 	}
 
 	override public function update(elapsed:Float)
