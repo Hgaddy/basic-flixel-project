b7ddf8e13225ae92d27f15a0c4c16bdc70948c38
Hgaddy <holdengaddy@gmail.com>
2022-02-22 23:34:44 -0600

    Added 'Hello World' text and edited its style.

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -10,8 +10,16 @@ class PlayState extends FlxState
 	override public function create()
 	{
 		super.create();
+
 		hero = new Hero(300,300);
 		add(hero);
+
+		var text = new flixel.text.FlxText(0, 0, 0, "Hello World", 64);
+		text.screenCenter();
+		text.borderColor = 0xFFFFC0CB;
+		text.borderSize = 200;
+		text.borderStyle = SHADOW;
+		add(text);
 	}
 
 	override public function update(elapsed:Float)
